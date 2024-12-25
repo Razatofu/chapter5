b72b1bb53727723363c788b75d66bb26d777606b
raza ayaz <razaayaz@yahoo.com>
2024-12-25 09:20:26 +0500

    Add files via upload

---
+++ abc.js
@@ -0,0 +1,24 @@
+var num1 = "a";
+var num2 = "b";
+console.log(num1);
+console.log(num2);
+alert(function add(num1,num2) { return num1 + num2
+    
+});
+
+
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  let result = add(3, 5); // Calling the function and storing the result
+  console.log(result); // Output: 8
+  
+
+document.write("sum of 3 and 5 is"  + result)
+
+
+  let a = 3;
+  let b = 5;
+  let sum = a+b; 
